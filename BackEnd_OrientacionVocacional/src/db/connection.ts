a98b032c699711d1712c13501daf7bc8f143efd2
Juan Araujo Saucedo <juanjaraujo21@gmail.com>
2024-08-31 00:24:41 -0500

    usando el host

--- BackEnd_OrientacionVocacional/src/db/connection.ts
+++ BackEnd_OrientacionVocacional/src/db/connection.ts
@@ -16,7 +16,8 @@ if (!DB_NAME || !DB_USER || !DB_PASSWORD || !DB_HOST) {
 
 
 const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
-    // host: DB_HOST,
+    
+   host: DB_HOST,
     dialect: 'postgres',
     dialectOptions: {
       ssl: {

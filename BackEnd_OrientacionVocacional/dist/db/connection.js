76cd761b17e89c9c643859aeda9719a767a7324a
Juan Araujo Saucedo <juanjaraujo21@gmail.com>
2024-08-31 00:33:58 -0500

    actualizar js

--- BackEnd_OrientacionVocacional/dist/db/connection.js
+++ BackEnd_OrientacionVocacional/dist/db/connection.js
@@ -7,12 +7,16 @@ const sequelize_1 = require("sequelize");
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
 const { DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DATABASE_URL } = process.env;
-if (!DB_NAME || !DB_USER || !DB_PASSWORD || !DB_HOST) {
-    throw new Error('Error al obtener credenciales de la DATABASE');
+// if (!DB_NAME || !DB_USER || !DB_PASSWORD || !DB_HOST) {
+//   throw new Error('Error al obtener credenciales de la DATABASE');
+// }
+// const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+//  host: DB_HOST,
+if (!DATABASE_URL) {
+    throw new Error('DATABASE_URL no está definido en las variables de entorno');
 }
-const sequelize = new sequelize_1.Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
-    // host: DB_HOST,
-    dialect: 'mysql',
+const sequelize = new sequelize_1.Sequelize(DATABASE_URL, {
+    dialect: 'postgres',
     dialectOptions: {
         ssl: {
             require: true,
